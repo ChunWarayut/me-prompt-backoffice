eaf505de771f01676ad3f5f9785f433b284102cd
ChunWarayut <warayut2555@gmail.com>
2023-03-05 00:12:27 +0700

    init project

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,16 @@
+import "bootstrap/dist/css/bootstrap.css"
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 
 export default function App({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+  useEffect(() => {
+    typeof document !== undefined
+      ? require("bootstrap/dist/js/bootstrap")
+      : null;
+  }, [router.events]);
+
   return <Component {...pageProps} />
 }
